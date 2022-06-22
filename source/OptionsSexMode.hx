f3be896ee9db2c84d99211e87ca74fc687de1cdc
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-22 18:49:04 -0300

    Update OptionsSexMode.hx

--- source/OptionsSexMode.hx
+++ source/OptionsSexMode.hx
@@ -122,6 +122,9 @@ class OptionsMenu extends MusicBeatState
 
 		FlxTween.tween(versionShit,{y: FlxG.height - 18},2,{ease: FlxEase.elasticInOut});
 		FlxTween.tween(blackBorder,{y: FlxG.height - 18},2, {ease: FlxEase.elasticInOut});
+                #if android
+                addVirtualPad(FULL, A_B);
+                #end
 
 		super.create();
 	}
